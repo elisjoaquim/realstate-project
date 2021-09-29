e48e38e28aab85fe85b9df5e8be7afe8125cfe6a
Elis Joaquim <elisjoaquim@gmail.com>
2021-09-29 17:16:44 +0100

    #pathcorrection

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,22 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWxpc2pvYXF1aW0iLCJhIjoiY2t1NW1jdGNnMXB1bTJvbzFsMzR0bHFkMyJ9.TQAfzfgyr1FikwdJ5X1QFQ',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      'pk.eyJ1IjoiZWxpc2pvYXF1aW0iLCJhIjoiY2t1NW1jdGNnMXB1bTJvbzFsMzR0bHFkMyJ9.TQAfzfgyr1FikwdJ5X1QFQ',
+  }
+).addTo(map);
+
+////////////////////////////////////////////////////
+/////////////////TESTEMONIAL SLIDER ////////////////
+////////////////////////////////////////////////////
 const slides = document.querySelectorAll('.testemonial__slide');
 const dotsContainer = document.querySelector('.testemonial__dots-container');
 let currentSlide = 0;
